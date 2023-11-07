94c1c8e18029863daef53cbbda3941d840ed914a
Desmatrix <desmatrixm31@gmail.com>
2023-11-07 20:15:48 +0100

    improved language and changed variables to float.

--- UltraSonic_Sensor_V_01.ino
+++ UltraSonic_Sensor_V_01.ino
@@ -1,35 +1,35 @@
-int Trig = 2;  //pin 2 Arduino połączony z pinem Trigger czujnika
-int Echo = 3;  //pin 3 Arduino połączony z pinem Echo czujnika
-int CM;        //odległość w cm
-long CZAS;     //długość powrotnego impulsu w uS
+int Trig = 2;  //pin 2 Arduino connected with Trigger pin in the sensor
+int Echo = 3;  //pin 3 Arduino connected with Echo pin in the sensor
+float CM;      //Distance in cm
+float CZAS;    //lengh of returning impulse in uS
 int incomingByte = 0; 
 
 void setup()
 {
-  Serial.begin(9600);                        //inicjalizaja monitora szeregowego
-  pinMode(Trig, OUTPUT);                     //ustawienie pinu 2 w Arduino jako wyjście
-  pinMode(Echo, INPUT);                      //ustawienie pinu 3 w Arduino jako wejście
-  pinMode(4, OUTPUT);                        //przypisanie pinu do LED
+  Serial.begin(9600);                        //iniciation of time port
+  pinMode(Trig, OUTPUT);                     //set pin 2 as output
+  pinMode(Echo, INPUT);                      //set pin 3 as input
+  pinMode(4, OUTPUT);                        //set pin 4 (connected led) as output
 
-  Serial.println("Test czujnika odleglosci");
+  
 }
 
-void pomiar_odleglosci ()
+void measure_distance ()          //code that will measure distance and save it as CM
 {
-  digitalWrite(Trig, LOW);        //ustawienie stanu wysokiego na 2 uS - impuls inicjalizujacy - patrz dokumentacja
+  digitalWrite(Trig, LOW);        //set low status
   delayMicroseconds(2);
-  digitalWrite(Trig, HIGH);       //ustawienie stanu wysokiego na 10 uS - impuls inicjalizujacy - patrz dokumentacja
+  digitalWrite(Trig, HIGH);       //set high status
   delayMicroseconds(10);
   digitalWrite(Trig, LOW);
   digitalWrite(Echo, HIGH); 
   CZAS = pulseIn(Echo, HIGH);
-  CM = CZAS / 58;   
-  }             //szerokość odbitego impulsu w uS podzielone przez 58 to odleglosc w cm - patrz dokumentacja
+  CM = CZAS / 58;   //lengh of returining signal divided by 58 gives the distance in cm
+  }             
 
 
 void loop()
 {
-  pomiar_odleglosci();              //pomiar odległości
+  measure_distance();            
 
   if (CM<100) 
   {  
@@ -37,11 +37,13 @@ void loop()
 
   Serial.println(CM);
 
-  }   //wyświetlanie wyników na ekranie w pętli co 800 ms
+  }   
   else
   {
   digitalWrite(4, LOW); // turn the LED off by making the voltage LOW  
   }
-  delay(800);
+  delay(800); //repeat the loop after given amount of mS
 }
   
+  
+
